e17093d3e66cbd9084e56457f729ae11df11530c
koala0000 <1487660836@qq.com>
2020-12-24 11:25:12 +0800

    fix:settings footerTxt

--- src/settings.js
+++ src/settings.js
@@ -42,7 +42,7 @@ module.exports = {
   /**
    * 底部文字，支持html语法
    */
-  footerTxt: '© 2019 Zheng Jie <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License 2.0</a>',
+  footerTxt: '© 2020 Marchsoft <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License 2.0</a>',
   /**
    * 备案号
    */
