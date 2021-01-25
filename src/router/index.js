281f6f07760aad9a7ba877340162bda2f3c221ab
lixiangxiang <204827740@qq.com>
2021-01-25 09:09:48 +0800

    fix:登录跳转404问题

--- src/router/index.js
+++ src/router/index.js
@@ -56,7 +56,7 @@ export const loadMenus = (next, to) => {
   buildMenus().then(res => {
     if (res.data) {
       const asyncRouter = filterAsyncRouter(res.data)
-      asyncRouter.push({ path: '*', redirect: '/404', hidden: true })
+      asyncRouter.push({ path: '*', redirect: '/', hidden: true })
       store.dispatch('GenerateRoutes', asyncRouter).then(() => { // 存储路由
         router.addRoutes(asyncRouter) // 动态添加可访问路由表
         next({ ...to, replace: true })
